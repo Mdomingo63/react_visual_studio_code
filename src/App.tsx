fd932e3dd68d17917ace760f6b37dc4a9938eb45
Domingo Sánchez <smatos63@gmail.com>
2026-08-20 14:20:56 -0400

    Incluir botón para ocultar sesion

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 
 import Formulario from './componentes/Formulario';
+import Galeria from './componentes/galeria';
 
 function App() {
   return (
     <div>
       <h1>Sistema de Nómina</h1>
+      <Galeria />
       <p>Tu proyecto React + TypeScript está listo para empezar.</p>
       <Formulario />
     </div>
---
+++ src/componentes/galeria.tsx
@@ -0,0 +1,22 @@
+
+function Galeria() {
+    return (
+        <>
+           <h1>Tareas Pendientes de Yenis Sánchez</h1>
+           <img 
+             src="https://i.imgur.com/yXOvdOSs.jpg"
+             alt="Yenis Sánchez"
+             className="photo"
+
+           />
+           <ul>
+             <li>Comprar leche</li>
+             <li>Realizar ejercicio</li>
+             <li>Leer un libro</li>
+           </ul>
+
+
+        </>
+    )
+}
+export default Galeria;
