1f63a953bf0452d80e2e2b9da1d78117bcdd283d
Domingo Sánchez <smatos63@gmail.com>
2026-08-20 14:25:49 -0400

    Correcciones

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-
+import { useState } from 'react';
 import Formulario from './componentes/Formulario';
 import Galeria from './componentes/galeria';
 
 function App() {
+  const [mostrarGaleria, setMostrarGaleria] = useState(true);
+
   return (
     <div>
       <h1>Sistema de Nómina</h1>
       <Formulario />
       <p>Tu proyecto React + TypeScript está listo para empezar.</p>
-      <Galeria />
+      <button onClick={() => setMostrarGaleria(!mostrarGaleria)}>
+        {mostrarGaleria ? 'Ocultar Galería' : 'Mostrar Galería'}
+      </button>
+      {mostrarGaleria && <Galeria />}
     </div>
   );
 }
